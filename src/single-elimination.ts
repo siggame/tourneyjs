382c4baf68a173efbc4d8c0c4a056619ad696db4
user404d <conduffql@gmail.com>
2017-02-17 06:38:47 -0600

    Fixing bug in scheduling

--- src/single-elimination.ts
+++ src/single-elimination.ts
@@ -6,201 +6,204 @@ import { EventEmitter } from "events";
 export type LiveMatch = [number, any[], Promise<Match>]
 
 export class SingleElimination {
-    playing: LiveMatch[];
-    ready: Match[];
-    results: Match[];
+  playing: LiveMatch[];
+  ready: Match[];
+  results: Match[];
 
-    private bronze_final: Bracket;
-    private play_handler: Function;
-    private play_timer: NodeJS.Timer;
-    private tournament_events: EventEmitter;
-    private winners: Bracket;
+  private bronze_final: Bracket;
+  private play_handler: Function;
+  private play_timer: NodeJS.Timer;
+  private tournament_events: EventEmitter;
+  private winners: Bracket;
 
-    constructor(teams: any[], with_bronze_final: boolean, randomize: boolean) {
-        if (teams.length == 0) {
-            throw new Error("Team list cannot be empty.");
-        }
-        else if (teams.length < 2) {
-            throw new Error("Not enough teams to form a tournament.");
-        }
-        else if (with_bronze_final && teams.length == 2) {
-            throw new Error("Not enough teams to have bronze final.");
-        }
-
-        this.winners = new Bracket(teams.length);
-        this.playing = [];
-        this.ready = [];
-        this.results = [this.winners.root];
-        this.tournament_events = new EventEmitter();
-
-        if (with_bronze_final) {
-            this.bronze_final = new Bracket(2);
-            this.bronze_final.dep = this.winners;
-            this.bronze_final.root.deps = this.winners.root.deps;
-
-            // add another parent for winners final deps
-            // to point to bronze final
-            this.winners.root.deps.forEach((match) => {
-                match.next.push(this.bronze_final.root);
-            });
-            this.results.push(this.bronze_final.root);
-        }
-
-        (randomize ? permute(teams) : teams).forEach((team, i) => {
-            // convert index to gray code
-            let position = i ^ (i >> 1);
-            let match: Match = this.winners.root;
-
-            // follow gray code binary digits into position
-            while (match.deps !== null) {
-                match = match.deps[position & 1];
-                position >>= 1;
-            }
-
-            // add team to match found
-            match.teams.push(team);
-
-            // add match to ready list unless already in list
-            if (this.ready.indexOf(match) < 0) {
-                this.ready.push(match);
-            }
-        });
-
-        // remove matches which are effectively "bye"s
-        let completed = 0;
-
-        Array(this.ready.length).fill(null).map((_, i) => {
-            const match = this.ready[i];
-            if (match.teams.length < 2) { // only one team, apply bye
-                const winner = match.teams[0];
-                const loser = match.teams[1];
-                match.meta_data = { "winner": winner, "loser": loser };
-                match.next[0].teams.push(winner);
-                if (match.next[1] !== undefined) {
-                    match.next[1].teams.push(loser);
-                }
-                this.ready[i] = this.ready[completed];
-                this.ready[completed] = match;
-                completed++;
-            }
-        });
-
-        this.ready = this.ready.slice(completed, this.ready.length);
-
-        this.tournament_events.on("ready", (match) => {
-            this.ready.push(match);
-            this.play_timer = setImmediate(_ => this.tournament_events.emit("play"));
-        }).on("done", _ => {
-            setImmediate(_ => {
-                if (Math.floor(Math.random() * 3) == 0) {
-                    this.playing = this.playing.filter((live_match) => {
-                        return live_match[2].then(match => {
-                            return match.meta_data === null;
-                        })
-                    });
-                }
-            });
-        }).on("finished?", _ => {
-            setImmediate((is_finished) => {
-                if (is_finished) {
-                    this.playing = [];
-                    this.ready = [];
-                    this.tournament_events.emit("on_finished");
-                }
-            }, this.results.reduce((accum, match) => {
-                return accum && match.meta_data !== null;
-            }, true));
-        }).on("error", (err) => {
-            this.pause();
-            console.error(err.stack);
-        });
+  constructor(teams: any[], with_bronze_final: boolean, randomize: boolean) {
+    if (teams.length == 0) {
+      throw new Error("Team list cannot be empty.");
+    }
+    else if (teams.length < 2) {
+      throw new Error("Not enough teams to form a tournament.");
+    }
+    else if (with_bronze_final && teams.length == 2) {
+      throw new Error("Not enough teams to have bronze final.");
     }
 
-    private make_live(match, fight) {
-        return new Promise((resolve, reject) => {
-            fight(match)
-                .then(result => {
-                    match.meta_data = result;
-                    return match;
-                })
-                .then(resolve)
-                .catch(reject)
+    this.winners = new Bracket(teams.length);
+    this.playing = [];
+    this.ready = [];
+    this.results = [this.winners.root];
+    this.tournament_events = new EventEmitter();
+
+    if (with_bronze_final) {
+      this.bronze_final = new Bracket(2);
+      this.bronze_final.dep = this.winners;
+      this.bronze_final.root.deps = this.winners.root.deps;
+
+      // add another parent for winners final deps
+      // to point to bronze final
+      this.winners.root.deps.forEach((match) => {
+        match.next.push(this.bronze_final.root);
+      });
+      this.results.push(this.bronze_final.root);
+    }
+
+    (randomize ? permute(teams) : teams).forEach((team, i) => {
+      // convert index to gray code
+      let position = i ^ (i >> 1);
+      let match: Match = this.winners.root;
+
+      // follow gray code binary digits into position
+      while (match.deps !== null) {
+        match = match.deps[position & 1];
+        position >>= 1;
+      }
+
+      // add team to match found
+      match.teams.push(team);
+
+      // add match to ready list unless already in list
+      if (this.ready.indexOf(match) < 0) {
+        this.ready.push(match);
+      }
+    });
+
+    // remove matches which are effectively "bye"s
+    let completed = 0;
+
+    Array(this.ready.length).fill(null).map((_, i) => {
+      const match = this.ready[i];
+      if (match.teams.length < 2) { // only one team, apply bye
+        const winner = match.teams[0];
+        const loser = match.teams[1];
+        match.meta_data = { "winner": winner, "loser": loser };
+        match.next[0].teams.push(winner);
+        if (match.next[1] !== undefined) {
+          match.next[1].teams.push(loser);
+        }
+        if (match.next[0].teams.length == 2) {
+          this.ready.push(match.next[0])
+        }
+        this.ready[i] = this.ready[completed];
+        this.ready[completed] = match;
+        completed++;
+      }
+    });
+
+    this.ready = this.ready.slice(completed, this.ready.length);
+
+    this.tournament_events.on("ready", (match) => {
+      this.ready.push(match);
+      this.play_timer = setImmediate(_ => this.tournament_events.emit("play"));
+    }).on("done", _ => {
+      setImmediate(_ => {
+        if (Math.floor(Math.random() * 3) == 0) {
+          this.playing = this.playing.filter((live_match) => {
+            return live_match[2].then(match => {
+              return match.meta_data === null;
+            })
+          });
+        }
+      });
+    }).on("finished?", _ => {
+      setImmediate((is_finished) => {
+        if (is_finished) {
+          this.playing = [];
+          this.ready = [];
+          this.tournament_events.emit("on_finished");
+        }
+      }, this.results.reduce((accum, match) => {
+        return accum && match.meta_data !== null;
+      }, true));
+    }).on("error", (err) => {
+      this.pause();
+      console.error(err.stack);
+    });
+  }
+
+  private make_live(match, fight) {
+    return new Promise((resolve, reject) => {
+      fight(match)
+        .then(result => {
+          match.meta_data = result;
+          return match;
         })
+        .then(resolve)
+        .catch(reject)
+    })
+  }
+
+  enqueue(id: number) {
+    const match = this.winners.matches[id];
+    if (this.ready.indexOf(match) < 0) {
+      this.ready.push(match);
+      return true;
+    }
+    return false;
+  }
+
+  play(fight, success, error) {
+    if (this.tournament_events.listeners("play").length !== 0) {
+      console.log("Tournament already playing.");
+      return;
+    }
+    else if (this.tournament_events.listenerCount("error") == 0) {
+      throw new Error("Tournament has been stopped.");
     }
 
-    enqueue(id: number) {
-        const match = this.winners.matches[id];
-        if (this.ready.indexOf(match) < 0) {
-            this.ready.push(match);
-            return true;
-        }
-        return false;
+    this.play_handler = _ => {
+      const live_matches = this.ready.map((match): LiveMatch => {
+        return [
+          match.id,
+          match.teams,
+          this.make_live(match, fight)
+            .then((match: Match) => {
+              if (!("winner" in match.meta_data && "loser" in match.meta_data)) {
+                throw new Error("Missing meta data.")
+              }
+              match.notify_next((next_match) => {
+                this.tournament_events.emit("ready", next_match);
+              }, _ => this.tournament_events.emit("finished?"));
+              success(match);
+              this.tournament_events.emit("done");
+              return match;
+            }).catch((err) => {
+              this.tournament_events.emit("error", err);
+              error(match, err);
+            })
+        ]
+      });
+
+      this.playing.concat(live_matches);
+      this.ready = [];
+    };
+
+    this.tournament_events.on("play", this.play_handler);
+    this.play_timer = setImmediate(_ => this.tournament_events.emit("play"));
+  }
+
+  pause() {
+    clearImmediate(this.play_timer);
+    this.tournament_events.removeListener("play", this.play_handler);
+  }
+
+  resume() {
+    if (this.tournament_events.listenerCount("play") > 0) {
+      console.log("Tournament already playing.");
+      return;
+    }
+    else if (this.tournament_events.listenerCount("error") == 0) {
+      throw new Error("Tournament has been stopped.");
     }
 
-    play(fight, success, error) {
-        if (this.tournament_events.listeners("play").length !== 0) {
-            console.log("Tournament already playing.");
-            return;
-        }
-        else if (this.tournament_events.listenerCount("error") == 0) {
-            throw new Error("Tournament has been stopped.");
-        }
+    this.tournament_events.on("play", this.play_handler);
+    this.play_timer = setImmediate(_ => this.tournament_events.emit("play"));
+  }
 
-        this.play_handler = _ => {
-            const live_matches = this.ready.map((match): LiveMatch => {
-                return [
-                    match.id,
-                    match.teams,
-                    this.make_live(match, fight)
-                        .then((match: Match) => {
-                            if (!("winner" in match.meta_data && "loser" in match.meta_data)) {
-                                throw new Error("Missing meta data.")
-                            }
-                            match.notify_next((next_match) => {
-                                this.tournament_events.emit("ready", next_match);
-                            }, _ => this.tournament_events.emit("finished?"));
-                            success(match);
-                            this.tournament_events.emit("done");
-                            return match;
-                        }).catch((err) => {
-                            this.tournament_events.emit("error", err);
-                            error(match, err);
-                        })
-                ]
-            });
+  stop() {
+    clearImmediate(this.play_timer);
+    this.tournament_events.removeAllListeners();
+  }
 
-            this.playing.concat(live_matches);
-            this.ready = [];
-        };
-
-        this.tournament_events.on("play", this.play_handler);
-        this.play_timer = setImmediate(_ => this.tournament_events.emit("play"));
-    }
-
-    pause() {
-        clearImmediate(this.play_timer);
-        this.tournament_events.removeListener("play", this.play_handler);
-    }
-
-    resume() {
-        if (this.tournament_events.listenerCount("play") > 0) {
-            console.log("Tournament already playing.");
-            return;
-        }
-        else if (this.tournament_events.listenerCount("error") == 0) {
-            throw new Error("Tournament has been stopped.");
-        }
-
-        this.tournament_events.on("play", this.play_handler);
-        this.play_timer = setImmediate(_ => this.tournament_events.emit("play"));
-    }
-
-    stop() {
-        clearImmediate(this.play_timer);
-        this.tournament_events.removeAllListeners();
-    }
-
-    once(event, callback) {
-        this.tournament_events.once(event, callback);
-    }
+  once(event, callback) {
+    this.tournament_events.once(event, callback);
+  }
 }
--- test/single-elimination.test.ts
+++ test/single-elimination.test.ts
@@ -2,149 +2,159 @@ import * as tourney from "../index";
 import * as should from "should";
 
 describe("Single Elimination", () => {
-    it("should be constructable", (done) => {
-        const t = new tourney.SingleElimination(["abc", "def", "ghi", "lmn", "opq"], false, false);
-        done();
-    });
+  it("should be constructable", (done) => {
+    const t = new tourney.SingleElimination(["abc", "def", "ghi", "lmn", "opq"], false, false);
+    done();
+  });
 
-    it("should allow bronze final", (done) => {
-        const t = new tourney.SingleElimination(["abc", "def", "ghi", "lmn", "opq"], true, false);
-        should(t.results.length).equal(2);
-        done();
-    });
+  it("should allow bronze final", (done) => {
+    const t = new tourney.SingleElimination(["abc", "def", "ghi", "lmn", "opq"], true, false);
+    should(t.results.length).equal(2);
+    done();
+  });
 
-    it("should allow randomization", (done) => {
-        const t = new tourney.SingleElimination(["abc", "def", "ghi", "lmn", "opq"], false, true);
-        done();
-    });
+  it("should allow randomization", (done) => {
+    const t = new tourney.SingleElimination(["abc", "def", "ghi", "lmn", "opq"], false, true);
+    done();
+  });
 
-    it("should not allow empty team lists", (done) => {
-        const bad = () => new tourney.SingleElimination([], false, false);
-        bad.should.throw("Team list cannot be empty.");
-        done();
-    });
+  it("should not allow empty team lists", (done) => {
+    const bad = () => new tourney.SingleElimination([], false, false);
+    bad.should.throw("Team list cannot be empty.");
+    done();
+  });
 
-    it("should not allow single team tournaments", (done) => {
-        const bad = () => { new tourney.SingleElimination(["ahhh"], false, false) };
-        bad.should.throw("Not enough teams to form a tournament.");
-        done();
-    });
+  it("should not allow single team tournaments", (done) => {
+    const bad = () => { new tourney.SingleElimination(["ahhh"], false, false) };
+    bad.should.throw("Not enough teams to form a tournament.");
+    done();
+  });
 
-    it("should not allow bronze final with two teams", (done) => {
-        const bad = () => { new tourney.SingleElimination(["ahhh", "woo"], true, false) };
-        bad.should.throw("Not enough teams to have bronze final.");
-        done();
-    });
+  it("should not allow bronze final with two teams", (done) => {
+    const bad = () => { new tourney.SingleElimination(["ahhh", "woo"], true, false) };
+    bad.should.throw("Not enough teams to have bronze final.");
+    done();
+  });
 
-    it("should allow tournaments with 2 teams", (done) => {
-        const t = new tourney.SingleElimination(Array(2).fill(null), false, false);
-        t.once("on_finished", () => {
-            should(t.ready).be.empty();
-            done();
-        });
+  it("should allow tournaments with 2 teams", (done) => {
+    const t = new tourney.SingleElimination(Array(2).fill(null), false, false);
+    t.once("on_finished", () => {
+      should(t.ready).be.empty();
+      done();
+    });
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , _ => { }, _ => { });
+  });
+
+  it("should be playable", (done) => {
+    const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , _ => { }, _ => { });
+    should(t.playing).not.be.empty;
+    done();
+  });
+
+  it("should finish tournament", (done) => {
+    const t = new tourney.SingleElimination(Array(1000).fill(null), true, true);
+    t.once("on_finished", () => {
+      should(t.ready).be.empty();
+      done();
+    });
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , _ => { }, _ => { });
+  });
+
+  it("should be pausable", (done) => {
+    const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , _ => { }, _ => { });
+    t.pause();
+    setImmediate(() => {
+      should(t.ready).not.be.empty();
+      done();
+    });
+  });
+
+  it("should be resumable", (done) => {
+    const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
+    t.once("on_finished", () => {
+      should(t.ready).be.empty();
+      done();
+    });
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , _ => { }, _ => { });
+    t.pause();
+    setImmediate(() => {
+      t.resume();
+    });
+  });
+
+  it("should be stoppable", (done) => {
+    const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , _ => { }, _ => { });
+    t.stop();
+    const bad = () => { t.resume() };
+    bad.should.throw("Tournament has been stopped.");
+    done();
+  });
+
+  it("should not be playable after stopping", (done) => {
+    const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , _ => { }, _ => { });
+    t.stop();
+    const bad = () => {
+      t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+        Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+        , _ => { }, _ => { });
+    };
+    bad.should.throw("Tournament has been stopped.");
+    done();
+  });
+
+  it("should pause tournament on error", (done) => {
+    const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , match => {
+        if (match.id === 20) throw new Error("borked")
+      }, (id, err) => {
+        done()
+      });
+  });
+
+  it("should recover tournament on error", (done) => {
+    const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
+    t.once("on_finished", _ => done());
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , match => {
+        if (match.id === 100) throw new Error("borked")
+      }, (match, err) => {
+        t.ready.push(match)
         t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , _ => { }, _ => { });
-    });
+          Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+          , match => {
+          }, (match, err) => {
+          });
+      });
+  });
 
-    it("should be playable", (done) => {
-        const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
-        t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , _ => { }, _ => { });
-        should(t.playing).not.be.empty;
-        done();
-    });
-
-    it("should finish tournament", (done) => {
-        const t = new tourney.SingleElimination(Array(1000).fill(null), true, true);
-        t.once("on_finished", () => {
-            should(t.ready).be.empty();
-            done();
-        });
-        t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , _ => { }, _ => { });
-    });
-
-    it("should be pausable", (done) => {
-        const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
-        t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , _ => { }, _ => { });
-        t.pause();
-        setImmediate(() => {
-            should(t.ready).not.be.empty();
-            done();
-        });
-    });
-
-    it("should be resumable", (done) => {
-        const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
-        t.once("on_finished", () => {
-            should(t.ready).be.empty();
-            done();
-        });
-        t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , _ => { }, _ => { });
-        t.pause();
-        setImmediate(() => {
-            t.resume();
-        });
-    });
-
-    it("should be stoppable", (done) => {
-        const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
-        t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , _ => { }, _ => { });
-        t.stop();
-        const bad = () => { t.resume() };
-        bad.should.throw("Tournament has been stopped.");
-        done();
-    });
-
-    it("should not be playable after stopping", (done) => {
-        const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
-        t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , _ => { }, _ => { });
-        t.stop();
-        const bad = () => {
-            t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-                Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-                , _ => { }, _ => { });
-        };
-        bad.should.throw("Tournament has been stopped.");
-        done();
-    });
-
-    it("should pause tournament on error", (done) => {
-        const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
-        t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , match => {
-                if (match.id === 20) throw new Error("borked")
-            }, (id, err) => {
-                done()
-            });
-    });
-
-    it("should recover tournament on error", (done) => {
-        const t = new tourney.SingleElimination(Array(1000).fill(null), false, false);
-        t.once("on_finished", _ => done());
-        t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-            Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-            , match => {
-                if (match.id === 100) throw new Error("borked")
-            }, (match, err) => {
-                t.ready.push(match)
-                t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
-                    Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
-                    , match => {
-                    }, (match, err) => {
-                    });
-            });
-    });
+  it("should finish tournament with more than 1/2 teams getting a bye", (done) => {
+    const t = new tourney.SingleElimination(Array(1025).fill(null), false, false);
+    t.once("on_finished", _ => done());
+    t.play((match, winner = Math.floor(Math.random() * 2) % 2) =>
+      Promise.resolve({ "winner": match.teams[winner], "loser": match.teams[winner ^ 1] })
+      , match => {
+      }, (match, err) => {
+      });
+  });
 });
