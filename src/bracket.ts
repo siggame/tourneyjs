8baf89d898ed4474e917c138b2c054fb18ef321e
user404d <conduffql@gmail.com>
2017-09-11 01:23:40 -0500

    Update order of bracket definitions

--- src/bracket.ts
+++ src/bracket.ts
@@ -1,15 +1,15 @@
 import { IMatch, Match } from "./match";
 
+export interface IBracket<T, U extends IMatch<T>> {
+  root: U;
+  dep?: IBracket<T, U>;
+  matches: U[];
+}
+
 export abstract class Bracket<T, U extends Match<T>> implements IBracket<T, U> {
   public root: U;
   public dep?: Bracket<T, U>;
   public matches: U[];
 
   constructor() { }
-}
-
-export interface IBracket<T, U extends IMatch<T>> {
-  root: U;
-  dep?: IBracket<T, U>;
-  matches: U[];
 }
