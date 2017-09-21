7d4973a5df8f172a6e64b228e36f4b8f43636134
user404d <conduffql@gmail.com>
2017-09-21 11:28:20 -0500

    Update most properties to be readonly

--- src/tournament.ts
+++ src/tournament.ts
@@ -6,25 +6,20 @@ import { IMatch, IMatchResult, Match, MatchResult } from "./match";
 export type TournamentStatus = "init" | "playing" | "paused" | "stopped";
 export type TournamentEvents = "error" | "finished" | "ready";
 
-export interface ITournamentPlayHandler<T> {
+export interface ITournamentUpdateHandler<T, U extends IMatch<T> = Match<T>> {
     (
-        fight: (match: IMatch<T>) => Promise<IMatchResult<T>>,
-        success: (match: IMatch<T>) => void,
-        error: (match: IMatch<T>, error: Error) => any,
-    )
-        : void;
-}
-
-export interface ITournamentEventHandler<T> {
-    (event: TournamentEvents, cb: (...args: any[]) => any): ITournament<T>;
+        fight: (match: U) => Promise<IMatchResult<T>>,
+        success: (match: U) => void,
+        error: (match: U, error: Error) => any,
+    ): void;
 }
 
 export interface ITournament<T, U extends IMatch<T> = Match<T>, V extends IBracket<T, U> = Bracket<T, U>>
     extends EventEmitter {
-    bracket: V;
-    status: TournamentStatus;
-    play: ITournamentPlayHandler<T>;
-    when: ITournamentEventHandler<T>;
+    readonly bracket: V;
+    readonly status: TournamentStatus;
+    readonly play: ITournamentUpdateHandler<T, U>;
+    when(event: TournamentEvents, cb: (...args: any[]) => any): ITournament<T>;
     pause(): void;
     resume(): void;
     stop(): void;
@@ -32,21 +27,20 @@ export interface ITournament<T, U extends IMatch<T> = Match<T>, V extends IBrack
 }
 
 export abstract class Tournament<T, U extends IMatch<T> = Match<T>, V extends IBracket<T, U> = Bracket<T, U>>
-    extends EventEmitter
-    implements ITournament<T, U, V>{
+    extends EventEmitter {
 
-    public bracket: V;
+    public readonly bracket: V;
 
     constructor(
-        public play: ITournamentPlayHandler<T> = () => { },
+        public readonly play: ITournamentUpdateHandler<T, U>,
         public status: TournamentStatus = "init",
     ) {
         super();
     }
 
-    pause(): void { }
-    resume(): void { }
-    stop(): void { }
+    abstract pause(): void;
+    abstract resume(): void;
+    abstract stop(): void;
     toString(): string { return `status: ${this.status}\n${this.bracket}`; }
-    when: ITournamentEventHandler<T> = (event, cb) => this.once(event, cb);
+    when(event: TournamentEvents, cb: (...args: any[]) => any) { return this.once(event, cb); }
 }
